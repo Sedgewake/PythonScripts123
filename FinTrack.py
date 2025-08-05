dd46a00bdaaea5b2ce8edace865e5f3a2d4db4f5
Sedgewake <evansj5125@gmail.com>
2025-08-05 10:46:26 +0300

    2025.08.05 10:46

--- FinTrack.py
+++ FinTrack.py
@@ -13,6 +13,29 @@ from matplotlib.style.core import available
 
 matplotlib.use("Agg")
 
+ACCOUNT = "Account"
+BALANCE = "Balance: "
+INCOME = "Income: "
+MAIN = "Main"
+HISTORY = "History"
+DIAGRAM = "Diagram"
+GRAPH = "Graph"
+ITEMS = "Items"
+ITEM = "Item: "
+TYPE = "Type: "
+PRICE = "Price: "
+COST = "Cost: "
+ADD = "Add"
+CHECKIN = "Check in"
+CHECKOUT = "Check out"
+DISMISS = "Dismiss"
+
+def from_cents(amount):
+    return float(amount / 100)
+
+def to_cents(amount):
+    return int(amount * 100.0)
+
 class Item:
     def __init__(self, name, item_type, price, count = 1):
         self.name = name
@@ -26,26 +49,26 @@ class ItemGUI:
         self.app_ref = app_ref
         self.my_item = my_item
         self.row = tk.Frame(self.parent, bg="#3a3a3a")
-        self.row.pack(fill="x", pady=3)
-        self.name_label = tk.Label(self.row, text=self.my_item.name, font=("Arial", 16), width=15, fg="white", bg="#3a3a3a")
+        self.row.pack(fill="x", pady=1)
+        self.name_label = tk.Label(self.row, text=self.my_item.name, font=("Arial", 14), width=15, fg="white", bg="#3a3a3a")
         self.name_label.pack(side="left", padx=5)
-        self.type_label = tk.Label(self.row, text=self.my_item.item_type, font=("Arial", 16), width=15, fg="white", bg="#3a3a3a")
+        self.type_label = tk.Label(self.row, text=self.my_item.item_type, font=("Arial", 14), width=15, fg="white", bg="#3a3a3a")
         self.type_label.pack(side="left")
-        self.price_input = tk.Entry(self.row, font=("Arial", 16), width=10, fg="white", bg="#3a3a3a")
+        self.price_input = tk.Entry(self.row, font=("Arial", 14), width=10, fg="white", bg="#3a3a3a")
         self.price_input.pack(side="left")
-        self.count_label = tk.Label(self.row, text=str(self.my_item.count), font=("Arial", 16), width=5, fg="white",bg="#3a3a3a")
-        self.total_cost_d = tk.Label(self.row, text=f"{cost_int_to_float(self.my_item.price * my_item.count):.2f}",font=("Arial", 16), width=15, fg="white", bg="#3a3a3a")
-        self.price_input.insert(tk.END, f"{cost_int_to_float(self.my_item.price):.2f}")
+        self.count_label = tk.Label(self.row, text=str(self.my_item.count), font=("Arial", 14), width=5, fg="white",bg="#3a3a3a")
+        self.total_cost_d = tk.Label(self.row, text=f"{from_cents(self.my_item.price * my_item.count):.2f}",font=("Arial", 16), width=15, fg="white", bg="#3a3a3a")
+        self.price_input.insert(tk.END, f"{from_cents(self.my_item.price):.2f}")
         self.price_input.bind("<KeyRelease>", self.change_item_price)
         self.total_cost_d.pack(side="left")
-        self.minus_btn = tk.Button(self.row, text="-", font=("Arial", 14), width=3, bg="#555555", fg="white",command=lambda: self.decrease_count())
+        self.minus_btn = tk.Button(self.row, text="-", font=("Arial", 10), width=3, bg="#555555", fg="white",command=lambda: self.decrease_count())
         self.minus_btn.pack(side="left", padx=5)
         self.count_label.pack(side="left")
-        plus_btn = tk.Button(self.row, text="+", font=("Arial", 14), width=3, bg="#555555", fg="white", command=lambda: self.increase_count())
+        plus_btn = tk.Button(self.row, text="+", font=("Arial", 10), width=3, bg="#555555", fg="white", command=lambda: self.increase_count())
         plus_btn.pack(side="left", padx=5)
 
     def change_item_price(self, event):
-        self.my_item.price = cost_float_to_int(float(self.price_input.get()))
+        self.my_item.price = to_cents(float(self.price_input.get()))
         self.update_item_display()
         self.app_ref.update_pending()
 
@@ -64,7 +87,7 @@ class ItemGUI:
             self.row.destroy()
             self.app_ref.item_list.remove(self.my_item)
             return
-        self.total_cost_d.configure(text=f"{cost_int_to_float(self.my_item.price * self.my_item.count):.2f}")
+        self.total_cost_d.configure(text=f"{from_cents(self.my_item.price * self.my_item.count):.2f}")
         self.count_label.configure(text=f"{self.my_item.count}")
 
 class Operation:
@@ -87,22 +110,13 @@ class Operation:
     @staticmethod
     def from_dict(data):
         timestamp = datetime.fromisoformat(data["timestamp"])
-        balance = data["balance"]
         items = [Item(**i) for i in data.get("items", [])]
-        return Operation(data["kind"], data["amount"], balance, timestamp, items)
-
-def cost_int_to_float(amount):
-    return float(amount / 100)
-
-
-def cost_float_to_int(amount):
-    return int(float(amount) * 100.0)
-
+        return Operation(data["kind"], data["amount"], data["balance"], timestamp, items)
 
 class FinanceCalcApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("Finance Calculator")
+        self.root.title("Finance Tracker")
         self.root.resizable(False, False)
         self.root.geometry("1000x800")
         self.root.configure(background="#323232")
@@ -119,6 +133,7 @@ class FinanceCalcApp:
         self.item_selector = None
         self.type_selector = None
         self.list_frame = None
+        self.account_frame = None
         self.history_frame = None
         self.available_items = []
         self.available_types = []
@@ -136,14 +151,17 @@ class FinanceCalcApp:
         #style.theme_use("clam")
         style.theme_use("default")
         style.configure("TNotebook", background="#323232", borderwidth=0)
-        style.configure("TNotebook.Tab", background="#323232", foreground="#e6e6e6", padding=(12, 8), font=('Segoe UI', 10, 'bold'))
+        style.configure("TNotebook.Tab", background="#323232", foreground="#e6e6e6", padding=(35, 7), font=('Segoe UI', 10, 'bold'))
         style.map("TNotebook.Tab", background=[("selected", "#3a3a3a")], foreground=[("selected", "#ffffff")])
         self.notebook = ttk.Notebook(root)
         self.notebook.pack(fill="both", expand=True, padx=2, pady=2)
 
+        self.notebook.bind("<<NotebookTabChanged>>", self.on_tab_selected)
+
         self.load_data()
 
         self.setup_tab_main()
+        self.setup_tab_items()
         self.setup_tab_history()
         self.setup_tab_pie()
         self.setup_tab_graph()
@@ -152,46 +170,52 @@ class FinanceCalcApp:
 
     def setup_tab_main(self):
         tab = tk.Frame(self.notebook, bg="#323232")
-        tk.Label(tab, textvariable=self.balance_var, font=("Arial", 20), fg="white", bg="#323232").pack(pady=5)
-        income_frame = tk.Frame(tab, bg="#323232")
+        self.account_frame = tk.LabelFrame(tab, fg="white", bg="#323232")
+        self.account_frame.pack(fill="x", padx=2, pady=2)
+        tk.Label(self.account_frame, textvariable=self.balance_var, font=("Arial", 20), fg="white", bg="#323232").pack(pady=5)
+        income_frame = tk.Frame(self.account_frame, bg="#323232")
         income_frame.pack(pady=5)
-        tk.Label(income_frame, text="Income:", font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
+        tk.Label(income_frame, text=INCOME, font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
         self.income_amount = tk.Entry(income_frame, font=("Arial", 20), width=15)
         self.income_amount.pack(side="left", padx=5)
         self.income_type = ttk.Combobox(income_frame, values=["Salary", "Gift", "Other", "Set"], style="Custom.TCombobox", font=("Arial", 20), height=20, width=10)
         self.income_type.set("Salary")
         self.income_type.pack(side="left", padx=5)
-        tk.Button(income_frame, text="Check In", font=("Arial", 14), command=self.add_income).pack(side="left", padx=5)
+        tk.Button(income_frame, text=CHECKIN, font=("Arial", 14), command=self.add_income).pack(side="left", padx=5)
         select_frame = tk.Frame(tab, bg="#323232")
         select_frame.pack(pady=5)
-        tk.Label(select_frame, text="Item:", font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
+        tk.Label(select_frame, text=ITEM, font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
         self.item_selector = ttk.Combobox(select_frame, values=[item.name for item in self.available_items], style="Custom.TCombobox", font=("Arial", 20), height=20, width=15)
         self.item_selector.set("")
         self.item_selector.pack(side="left")
         self.item_selector.bind("<<ComboboxSelected>>", self.item_change)
-        tk.Label(select_frame, text="Type:", font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
+        tk.Label(select_frame, text=TYPE, font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
         self.type_selector = ttk.Combobox(select_frame, values=[item for item in self.available_types], style="Custom.TCombobox", font=("Arial", 20), height=20, width=12)
         self.type_selector.set("")
         self.type_selector.pack(side="left")
-        tk.Label(select_frame, text="Price:", font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
+        tk.Label(select_frame, text=PRICE, font=("Arial", 20), fg="white", bg="#323232").pack(side="left")
         self.item_price_input = tk.Entry(select_frame, font=("Arial", 20), width=10)
         self.item_price_input.pack(side="left", padx=5)
-        tk.Button(select_frame, text="Add", font=("Arial", 14), command=self.add_item).pack(side="left", padx=5)
+        tk.Button(select_frame, text=ADD, font=("Arial", 14), command=self.add_item).pack(side="left", padx=5)
 
         self.list_frame = tk.Frame(tab, bg="#323232")
         self.list_frame.pack(pady=5)
 
         self.list_buttons_frame = tk.Frame(tab, bg="#323232")
         self.list_buttons_frame.pack(pady=5)
-        self.list_cost_label = tk.Label(self.list_buttons_frame, text=f"Cost: {float(self.pending / 100):.2f} ₽", font=("Arial", 20), fg="white", bg="#323232")
+        self.list_cost_label = tk.Label(self.list_buttons_frame, text=COST + f"{float(self.pending / 100):.2f} ₽", font=("Arial", 20), fg="white", bg="#323232")
         self.list_cost_label.pack(side="left")
-        self.list_btn_ok = tk.Button(self.list_buttons_frame, text="Check Out", font=("Arial", 14), command=self.check_out)
+        self.list_btn_ok = tk.Button(self.list_buttons_frame, text=CHECKOUT, font=("Arial", 14), command=self.check_out)
         self.list_btn_ok.pack(side="left", padx=5)
-        self.list_btn_cancel = tk.Button(self.list_buttons_frame, text="Dismiss", font=("Arial", 14), command=self.dismiss_list)
+        self.list_btn_cancel = tk.Button(self.list_buttons_frame, text=DISMISS, font=("Arial", 14), command=self.dismiss_list)
         self.list_btn_cancel.pack(side="left", padx=5)
         self.notebook.add(tab, text="Main")
         self.update_pending()
         self.refresh_items()
+
+    def setup_tab_items(self):
+        tab = tk.Frame(self.notebook, bg="#323232")
+        self.notebook.add(tab, text=ITEMS)
 
 
     def setup_tab_history(self):
@@ -232,7 +256,7 @@ class FinanceCalcApp:
                             w.destroy()
                         frame.expanded = False
                     else:
-                        frame.extra = [tk.Label(frame, text=f"  - {i.name} ({i.count}) ({cost_int_to_float(i.price * i.count):.2f})", fg="white", bg="#444") for i in items]
+                        frame.extra = [tk.Label(frame, text=f"  - {i.name} ({i.count}) ({from_cents(i.price * i.count):.2f})", fg="white", bg="#444") for i in items]
                         for w in frame.extra:
                             w.pack(anchor="w")
                         frame.expanded = True
@@ -282,13 +306,13 @@ class FinanceCalcApp:
         if self.income_type.get() == "Set":
             b1 = self.balance
             self.balance = amount
-            self.operations.append(Operation("Set balance", cost_int_to_float(self.balance - b1), cost_int_to_float(self.balance)))
+            self.operations.append(Operation("Set balance", from_cents(self.balance - b1), from_cents(self.balance)))
         else:
             self.balance += amount
             if amount > 0:
-                self.operations.append(Operation("Income ("+self.income_type.get()+")", cost_int_to_float(amount), cost_int_to_float(self.balance)))
+                self.operations.append(Operation("Income ("+self.income_type.get()+")", from_cents(amount), from_cents(self.balance)))
             else:
-                self.operations.append(Operation("Expense (" + self.income_type.get() + ")", cost_int_to_float(amount), cost_int_to_float(self.balance)))
+                self.operations.append(Operation("Expense (" + self.income_type.get() + ")", from_cents(amount), from_cents(self.balance)))
         self.income_amount.delete(0, tk.END)
         self.update_main_tab()
         self.refresh_history()
@@ -307,7 +331,7 @@ class FinanceCalcApp:
             self.list_btn_cancel.pack_forget()
 
     def add_item(self):
-        i1 = Item(self.item_selector.get(), self.type_selector.get(), cost_float_to_int(float(self.item_price_input.get())))
+        i1 = Item(self.item_selector.get(), self.type_selector.get(), to_cents(float(self.item_price_input.get())))
         ItemGUI(self.list_frame, self, i1)
         self.item_list.append(i1)
         self.update_pending()
@@ -329,7 +353,7 @@ class FinanceCalcApp:
 
     def check_out(self):
         self.balance -= self.pending
-        self.operations.append(Operation("Purchase", cost_int_to_float(self.pending), cost_int_to_float(self.balance), None, self.item_list.copy()))
+        self.operations.append(Operation("Purchase", from_cents(self.pending), from_cents(self.balance), None, self.item_list.copy()))
         for widget in self.list_frame.winfo_children():
             widget.destroy()
         self.item_list.clear()
@@ -347,6 +371,28 @@ class FinanceCalcApp:
         for item in self.item_list:
             self.pending += item.price * item.count
         self.update_main_tab()
+        if len(self.item_list) > 17:
+            self.root.resizable(False, True)
+
+    def on_tab_selected(self, event):
+        selected_tab = event.widget.select()
+        tab_index = event.widget.index(selected_tab)
+        tab_name = self.notebook.tab(selected_tab, "text")
+
+        if tab_name == "History":
+            print("History tab selected")
+            self.refresh_history()
+        elif tab_name == "Main":
+            print("Main tab selected")
+        elif tab_name == "Items":
+            print("Items tab selected")
+            self.refresh_items()
+        elif tab_name == "Chart":
+            print("Chart tab selected")
+            # self.update_chart()  ← if you add such a method
+        elif tab_name == "Graph":
+            print("Graph tab selected")
+            # self.update_graph()  ← if you add such a method
 
     def item_change(self, event):
         if len(self.available_items) < 1:
